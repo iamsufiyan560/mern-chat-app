2fb5e0517c5040640e5aa8b25f740daf51117f1d
iamsufiyan560 <140635114+iamsufiyan560@users.noreply.github.com>
2024-08-12 20:16:27 +0530

    Backend completed

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
+import connectToMongoDB from "./db/connectToMongoDB.js";
+
+dotenv.config();
+connectToMongoDB();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.send("Hey Brother !");
+});
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
+
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
+});
